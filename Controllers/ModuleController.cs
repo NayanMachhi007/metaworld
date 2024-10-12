e4ac0e301d6d46792d3ebb08a5b026656058caee
NayanMachhi007 <nayankumarmachhi05@gmail.com>
2024-10-12 11:01:19 +0530

    update the data

---
+++ Controllers/ModuleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meta_Ads_World.Controllers
+{
+    public class ModuleController : Controller
+    {
+        public IActionResult ModuleLibrary()
+        {
+            return View();
+        }
+    }
+}
